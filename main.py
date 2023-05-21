8c607302c0d822490bc98d0a653e04502ce6151b
TanKaiYing <132726365+TanKaiYing@users.noreply.github.com>
2023-05-21 21:26:04 +0800

    main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ st.markdown(
 
 with header: 
     st.title('Welcome to Harvest Fields for Crop Prediction Service')
-    st.text('We specialize in providing excellent agricultural services. We offer various farming information services, including crop prediction. We provide the highest quality services to both small and large-scale farms. At Harvest Fields, we believe in using the latest technologies to ensure the highest quality results. We strive to ensure the long-term success of our clients and our business.')
+    st.markdown('We specialize in providing excellent agricultural services. We offer various farming information services, including crop prediction. We provide the highest quality services to both small and large-scale farms. At Harvest Fields, we believe in using the latest technologies to ensure the highest quality results. We strive to ensure the long-term success of our clients and our business.')
 
 with dataset:
     st.header('Crop Dataset')
@@ -43,38 +43,46 @@ with dataset:
 with features: 
     st.header('Features used for prediction')
     
-    st.markdown('* **First feature:** Temperature')
-    st.markdown('* **Second feature:** pH Value of Soil')
-    st.markdown('* **Third feature:** Types of Soil')
+    st.markdown('* Amount of Nitrogen needed in the soil')
+    st.markdown('* Amount of Phosphorous needed in the soil')
+    st.markdown('* Amount of Potassium needed in the soil')
+    st.markdown('* Surrounding Temperature')
+    st.markdown('* Humidity')
+    st.markdown('* Amount of Rainfall')
+    st.markdown('* pH value of the soil')
+    st.markdown('* State')
+    st.markdown('* Types of the soil')
     
 with model_training:
     st.header('Time to make your crop prediction!')
-    st.text('Here you get to choose the hyperparameters of the model and see how the performance change')
+    st.text('Here you get to choose the hyperparameters of the model and see how the performance change') 
 
     sel_col, disp_col = st.columns(2)
     
-    TEMPERATURE = sel_col.slider('What should be the Temperature of the model?', min_value=6, max_value=50, value=20, step=1)
+    N_SOIL = sel_col.slider('Amount of Nitrogen needed in the soil', min_value=0, max_value=150, value=20, step=5)
+    
+    P_SOIL = sel_col.slider('Amount of Phosphorous needed in the soil', min_value=0, max_value=150, value=20, step=5)
+    
+    K_SOIL = sel_col.slider('Amount of Potassium needed in the soil', min_value=0, max_value=150, value=20, step=5)
+    
+    TEMPERATURE = sel_col.slider('Surrounding Temperature', min_value=5, max_value=50, value=20, step=1)
+    
+    HUMIDITY = sel_col.slider('Humidity', min_value=0, max_value=100, value=20, step=5)
+    
+    RAINFALL = sel_col.slider('Amount of Rainfall', min_value=0, max_value=100, value=20, step=1)
+    
+    ph = sel_col.slider('pH value of the soil', min_value=1, max_value=9, value=20, step=1)
+    
+    STATE = sel_col.selectbox('State', options=['Kerala', 'Haryana', 'Himachal Pradesh', 'Uttar Pradesh', 'Assam', 'Maharashtra', 'Manipur', 'Madhya Pradesh', 'Gujarat', 'West Bengal', 'Uttrakhand', 'Meghalaya', 'Odisha', 'Nagaland', 'Andhra Pradesh', 'Punjab', 'Tamil Nadu', 'Tripura', 'Telangana', 'Rajasthan', 'Karnataka', 'Pondicherry', 'Goa', 'Chattisgarh', 'Jammu and Kashmir'
+], index = 0)
 
-    ph = sel_col.selectbox('How is the ph-value for plant?', options=[1,2,3,4,5,6,7,8,9], index = 0)
-    
-    sel_col.text('Here is list of features in ours data:')
-    sel_col.write(crop_data.columns)
-    
-    input_feature = sel_col.text_input('Which feature should be used as the input feature?','K_SOIL')
-    
-    regr = RandomForestRegressor()
-    
-    X = crop_data[[input_feature]]
-    y = crop_data['K_SOIL']
-    
-    regr.fit(X, y)
-    prediction = regr.predict(X)
-    
-    disp_col.subheader('Mean absolute error of the model is:')
-    disp_col.write(mean_absolute_error(y, prediction))
-    
-    disp_col.subheader('Mean squared error of the model is:')
-    disp_col.write(mean_squared_error(y, prediction))
-    
-    disp_col.subheader('R-squared score of the model is:')
-    disp_col.write(r2_score(y, prediction))
+    SOIL_TYPE = sel_col.selectbox('Type of Soil', options=['Sandy Clay loam', 'Desert soil', 'Sandy loam', 'Alluvial soil', 'Laterite soil', 'Regur soil', 'Sandy soil', 'Inceptisols', 'Black soil', 'Desert soils', 'Mountain soil', 'Loamy soil', 'Red soil', 'Delta alluvium', 'Clayey soils'], index = 0)
+
+#prediction code
+if st.button('Predict'):
+    makeprediction = crop_data.predict([[N_SOIL, P_SOIL, K_SOIL, TEMPERATURE, HUMIDITY, RAINFALL, ph, STATE, SOIL_TYPE]])
+    output=round(makeprediction[0],2)
+    st.success('Field conditions are most suitable for {}'.format(output))
+
+if _name_=='_main_':
+    main()
